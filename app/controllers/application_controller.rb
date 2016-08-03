0c95a9f430ccef83db74e35fe510a2519d9b188b
clupin <cristian.espinozal@usach.cl>
2016-08-02 22:26:13 -0400

    modificado application_controller para recibir mas datos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   	protected
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:username, :name, :lastname, :fono, :avatar, :email, :password, :password_confirmation)}
-		devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:username, :name, :lastname, :fono, :avatar, :email, :password, :password_confirmation, :current_password)}
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :lastname, :fono, :avatar, :email, :password, :password_confirmation])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :lastname, :fono, :avatar, :email, :password, :password_confirmation, :current_password])
 	end
 
 	def authenticate_normal_user!
